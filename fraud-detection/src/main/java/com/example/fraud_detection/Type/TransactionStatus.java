930433dfe1977b65aaf203fa7c98a492d30ec06e
hoanglv1106 <hoanglv1106@gmail.com>
2026-01-09 09:33:34 +0700

    Update MANUAL_REVIEW comment in TransactionStatus

--- fraud-detection/src/main/java/com/example/fraud_detection/Type/TransactionStatus.java
+++ fraud-detection/src/main/java/com/example/fraud_detection/Type/TransactionStatus.java
@@ -5,5 +5,5 @@ public enum TransactionStatus {
     PENDING,        // Đang chờ xử lý
     APPROVED,       // Đã duyệt thành công
     REJECTED_FRAUD, // Bị hệ thống chặn tự động
-    MANUAL_REVIEW   // <--- THÊM MỚI: Chờ Admin duyệt tay
+    MANUAL_REVIEW   // Chờ Admin duyệt tay
 }
